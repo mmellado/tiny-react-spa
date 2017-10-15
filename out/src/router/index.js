937ea6dadfb3ec1738b2071479c959e126e7a5ea
Marcos Mellado <marcos@mellados.com>
2017-10-15 12:50:18 +0200

    Add Switch to Router

--- out/src/router/index.js
+++ out/src/router/index.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from '../containers/Layout';
 import Home from '../containers/Home';
 import About from '../containers/About';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Layout>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </Layout>
+        <Switch>
+          <Layout>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Layout>
+        </Switch>
       </BrowserRouter>
     );
   }
